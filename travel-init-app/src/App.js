bc81cc50d2175b5207d18be9ed9d3f5e375dc435
Alexandra Hermeneanu <c-ahermeneanu@talentsoft.com>
2023-02-18 21:59:05 +0200

    Made a GET request to the server with Axios and JS fetch; simple display of a Weather Forecast

--- travel-init-app/src/App.js
+++ travel-init-app/src/App.js
@@ -1,10 +1,39 @@
-import Header from './components/Header'
+import Axios from 'axios'
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const name = 'Alexandra';
+  const [weatherForecast, setWeatherForecast] = useState([])
+  useEffect(() => {
+    fetchweatherForecast();
+  }, [])
+
+  useEffect(() => {
+    console.log(weatherForecast)
+  }, [weatherForecast])
+
+  const fetchweatherForecast = async () => {
+    const url = 'https://localhost:7109/WeatherForecast';
+    const response = await Axios(url);
+    setWeatherForecast(response.data);
+    // fetch(url)
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     setWeatherForecast(data)
+    // });
+  }
   return (
-    <div className="container">
-      <Header title={name} />
+    <div className="App">
+      {
+        weatherForecast && weatherForecast.map((weather, index) => {
+          return (
+            <div key={index} style={{ alignItems: 'center', margin: '20px 60px' }}>
+              <p>Forecast of the day: <span>{new Date(weather.date).toLocaleDateString('en-Gb')}</span></p>
+              <h4>{weather.temperatureC} C° | {weather.temperatureF} F° which means it's {weather.summary} outside.</h4>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
